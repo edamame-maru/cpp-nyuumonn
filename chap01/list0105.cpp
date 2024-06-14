423099d878ceddcfef0195c437fa13b7f4bf204b
edamame-maru <so1maru.ueda@gmail.com>
2024-06-14 22:11:24 +0100

    added files

---
+++ chap01/list0105.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "ABCDEFGHIJKLMNOPQRSTUVWXYZ" // 空白類をはさんでならんだ
+            "abcdefghijklmnopqrstuvwxyz\n"; // 文字列リテラルは連結される
+}
---
+++ chap01/list0106.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "１８と６３の和は" << 18 + 63 << "です。\n";
+}
